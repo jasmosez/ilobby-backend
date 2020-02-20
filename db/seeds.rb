08d94b3af74de7ef19442ae8c5c3c18409204335
jasmosez <jas.schaffer@gmail.com>
2020-02-20 16:59:53 -0500

    mid-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,93 @@
+require 'byebug'
+require 'rest-client'
+require 'json'
 
+url = "https://openstates.org/graphql"
+
+ny_senate = "ocd-organization/8291a233-623d-40e8-882d-21ec2d382c87"
+ny_assembly = "ocd-organization/26bb6306-85f0-4d10-bff7-d1cd5bdc0865"
+tx_senate = ""
+tx_assembly = ""
+
+legislator_query = '{
+  people(first: 100, memberOf: "ocd-organization/8291a233-623d-40e8-882d-21ec2d382c87") {
+    edges {
+      node {
+        name
+        image
+        party: currentMemberships(classification:"party") {
+          organization {
+            name
+
+          }
+        }
+        chamber: currentMemberships(classification:["upper", "lower"]) {
+          post {
+            label
+            role
+          }
+          organization {
+            name
+          }
+        }
+        committees: currentMemberships(classification:"committee") {
+          organization {
+            name
+          }
+        }
+        contactDetails {
+        	type
+          value
+          note
+        }
+      }
+    }
+  }
+}'
+
+payload = {
+  query: legislator_query
+}
+
+headers = {
+  "X-API-KEY": ENV["OS_KEY"]
+}
+
+response = RestClient.post(url, payload, headers )
+json = JSON.parse(response)
+puts json
 byebug
 0
 
 
-# RestClient.post(url, payload, headers={})
-# ENV[“EXAMPLE_API_KEY”]
+
+# GraphiQL query object to get all legislatures and their children
+# {
+#   jurisdictions {
+#     edges {
+#       node {
+#         id
+#         name
+#         organizations(first: 50, classification: "legislature") {
+#           edges {
+#             node {
+#               id
+#               name
+#               classification
+#               children(first: 5) {
+#                 edges {
+#                   node {
+#                     id
+#                     name
+#                     classification
+#                   }
+#                 }
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
