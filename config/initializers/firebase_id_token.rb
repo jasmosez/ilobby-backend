2bbccd6604d7312fc85a68bc1399bd4ebf7fa7b0
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2021-03-14 14:30:30 -0700

    Update firebase_id_token.rb

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,5 +1,5 @@
 FirebaseIdToken.configure do |config|
   config.project_ids = ['fir-react-starter-83bea']
   # config.redis = Redis.new(host: '10.0.1.1', port: 6380, db: 15)
-  config.redis = Redis.new(host: 'localhost', port: 6380, db: 15)
+  config.redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 end
