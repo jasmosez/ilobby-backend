04efcbdff2cb048f299cecbcb1a203d814289fb3
jamesmorrisschaffer <james.schaffer@datasociety.com>
2021-04-02 00:58:30 -0400

    TMC-5 firebaseIdToken config

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,5 +1,5 @@
 FirebaseIdToken.configure do |config|
-  config.project_ids = ['fir-react-starter-83bea']
+  config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
   # config.redis = Redis.new(host: '10.0.1.1', port: 6380, db: 15)
   if ENV["REDIS_URL"]
     config.redis = Redis.new(url: ENV["REDIS_URL"])
